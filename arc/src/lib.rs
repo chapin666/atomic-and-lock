11d1be9252f27564391591a77c26ed3a641d8df2
chapin666 <chengbin@mshare.cn>
2023-02-20 22:50:26 +0800

    feat: optimization arc

--- arc/src/lib.rs
+++ arc/src/lib.rs
@@ -1,9 +1,11 @@
 pub mod basic_arc;
+pub mod optimization_arc;
 pub mod weak_arc;
 
 #[cfg(test)]
 mod tests {
     use crate::basic_arc::Arc;
+    use crate::optimization_arc::Arc as OptimizationArc;
     use crate::weak_arc::Arc as WeakArc;
     use std::sync::atomic::AtomicUsize;
     use std::sync::atomic::Ordering::Relaxed;
@@ -55,6 +57,44 @@ mod tests {
         let x = WeakArc::new(("hello", DetectDrop));
         let y = WeakArc::downgrade(&x);
         let z = WeakArc::downgrade(&x);
+
+        let t = std::thread::spawn(move || {
+            // Weak pointer should be upgradable at this point.
+            let y = y.upgrade().unwrap();
+            assert_eq!(y.0, "hello");
+        });
+        assert_eq!(x.0, "hello");
+        t.join().unwrap();
+
+        // The data shouldn't be dropped yet,
+        // and the weak pointer should be upgradable.
+        assert_eq!(NUM_DROPS.load(Relaxed), 0);
+        assert!(z.upgrade().is_some());
+
+        drop(x);
+
+        // Now, the data should be dropped, and the
+        // weak pointer should no longer be upgradable.
+        assert_eq!(NUM_DROPS.load(Relaxed), 1);
+        assert!(z.upgrade().is_none());
+    }
+
+    #[test]
+    fn test_optimization_arc() {
+        static NUM_DROPS: AtomicUsize = AtomicUsize::new(0);
+
+        struct DetectDrop;
+
+        impl Drop for DetectDrop {
+            fn drop(&mut self) {
+                NUM_DROPS.fetch_add(1, Relaxed);
+            }
+        }
+
+        // Create an Arc with two weak pointers.
+        let x = OptimizationArc::new(("hello", DetectDrop));
+        let y = OptimizationArc::downgrade(&x);
+        let z = OptimizationArc::downgrade(&x);
 
         let t = std::thread::spawn(move || {
             // Weak pointer should be upgradable at this point.
---
+++ arc/src/optimization_arc.rs
@@ -0,0 +1,153 @@
+use std::cell::UnsafeCell;
+use std::mem::ManuallyDrop;
+use std::ops::Deref;
+use std::ptr::NonNull;
+use std::sync::atomic::Ordering::{Acquire, Relaxed, Release};
+use std::sync::atomic::{fence, AtomicUsize};
+
+struct ArcData<T> {
+    data_ref_count: AtomicUsize,
+    alloc_ref_count: AtomicUsize,
+    data: UnsafeCell<ManuallyDrop<T>>,
+}
+
+pub struct Arc<T> {
+    ptr: NonNull<ArcData<T>>,
+}
+unsafe impl<T: Sync + Send> Send for Arc<T> {}
+unsafe impl<T: Sync + Send> Sync for Arc<T> {}
+
+impl<T> Arc<T> {
+    pub fn new(data: T) -> Arc<T> {
+        Arc {
+            ptr: NonNull::from(Box::leak(Box::new(ArcData {
+                alloc_ref_count: AtomicUsize::new(1),
+                data_ref_count: AtomicUsize::new(1),
+                data: UnsafeCell::new(ManuallyDrop::new(data)),
+            }))),
+        }
+    }
+
+    fn data(&self) -> &ArcData<T> {
+        unsafe { self.ptr.as_ref() }
+    }
+
+    pub fn downgrade(arc: &Self) -> Weak<T> {
+        let mut n = arc.data().alloc_ref_count.load(Relaxed);
+        loop {
+            if n == usize::MAX {
+                std::hint::spin_loop();
+                n = arc.data().alloc_ref_count.load(Relaxed);
+                continue;
+            }
+            assert!(n < usize::MAX - 1);
+            if let Err(e) =
+                arc.data()
+                    .alloc_ref_count
+                    .compare_exchange_weak(n, n + 1, Acquire, Relaxed)
+            {
+                n = e;
+                continue;
+            }
+            return Weak { ptr: arc.ptr };
+        }
+    }
+
+    pub fn get_mut(arc: &mut Self) -> Option<&mut T> {
+        if arc
+            .data()
+            .alloc_ref_count
+            .compare_exchange(1, usize::MAX, Acquire, Relaxed)
+            .is_err()
+        {
+            return None;
+        }
+        let is_unique = arc.data().data_ref_count.load(Relaxed) == 1;
+        arc.data().alloc_ref_count.store(1, Release);
+        if !is_unique {
+            return None;
+        }
+        fence(Acquire);
+        unsafe { Some(&mut *arc.data().data.get()) }
+    }
+}
+
+impl<T> Deref for Arc<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        unsafe { &*self.data().data.get() }
+    }
+}
+
+impl<T> Clone for Arc<T> {
+    fn clone(&self) -> Self {
+        if self.data().data_ref_count.fetch_add(1, Relaxed) > usize::MAX / 2 {
+            std::process::abort();
+        }
+        Arc { ptr: self.ptr }
+    }
+}
+
+impl<T> Drop for Arc<T> {
+    fn drop(&mut self) {
+        if self.data().data_ref_count.fetch_sub(1, Release) == 1 {
+            fence(Acquire);
+            unsafe {
+                ManuallyDrop::drop(&mut *self.data().data.get());
+            }
+            drop(Weak { ptr: self.ptr });
+        }
+    }
+}
+
+pub struct Weak<T> {
+    ptr: NonNull<ArcData<T>>,
+}
+unsafe impl<T: Sync + Send> Send for Weak<T> {}
+unsafe impl<T: Sync + Send> Sync for Weak<T> {}
+
+impl<T> Weak<T> {
+    fn data(&self) -> &ArcData<T> {
+        unsafe { self.ptr.as_ref() }
+    }
+
+    pub fn upgrade(&self) -> Option<Arc<T>> {
+        let mut n = self.data().data_ref_count.load(Relaxed);
+        loop {
+            if n == 0 {
+                return None;
+            }
+            assert!(n < usize::MAX);
+            if let Err(e) =
+                self.data()
+                    .data_ref_count
+                    .compare_exchange_weak(n, n + 1, Relaxed, Relaxed)
+            {
+                n = e;
+                continue;
+            }
+            return Some(Arc { ptr: self.ptr });
+        }
+    }
+}
+
+impl<T> Clone for Weak<T> {
+    fn clone(&self) -> Self {
+        if self.data().alloc_ref_count.fetch_add(1, Relaxed) > usize::MAX / 2 {
+            std::process::abort();
+        }
+        Weak { ptr: self.ptr }
+    }
+}
+
+impl<T> Drop for Weak<T> {
+    fn drop(&mut self) {
+        if self.data().alloc_ref_count.fetch_sub(1, Release) == 1 {
+            fence(Acquire);
+            unsafe {
+                drop(Box::from_raw(self.ptr.as_ptr()));
+            }
+        }
+    }
+}
