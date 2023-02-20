e1194c2e427a1d0ba6b240d66bb7e868ea7e9ce9
chapin666 <chengbin@mshare.cn>
2023-02-20 14:25:51 +0800

    feat: oneshot channel

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
+pub mod avoidalloc_channel;
+pub mod block_channel;
 pub mod channel;
 pub mod oneshot_channel;
+pub mod safe_channel;
 pub mod spin_lock;
 
 #[cfg(test)]
 mod tests {
+    use crate::avoidalloc_channel::Channel as AvoidallocChannel;
+    use crate::block_channel::Channel as BlockChannel;
     use crate::channel::Channel;
     use crate::oneshot_channel::Channel as OneshotChannel;
+    use crate::safe_channel::channel;
     use crate::spin_lock::SpinLock;
     use std::process;
     use std::thread;
@@ -55,12 +61,64 @@ mod tests {
         thread::scope(|s| {
             s.spawn(|| {
                 channel.send("hello world!");
+                channel.send("hello world2!");
                 t.unpark();
             });
             while !channel.is_ready() {
                 thread::park();
             }
             assert_eq!(channel.receive(), "hello world!");
+        });
+    }
+
+    #[test]
+    fn test_safe_channel() {
+        thread::scope(|s| {
+            let (sender, receiver) = channel();
+            let t = thread::current();
+            s.spawn(move || {
+                sender.send("hello world!");
+                t.unpark();
+            });
+
+            while !receiver.is_ready() {
+                thread::park();
+            }
+            assert_eq!(receiver.receive(), "hello world!");
+        });
+    }
+
+    #[test]
+    fn test_avoidalloc_channel() {
+        let mut channel = AvoidallocChannel::new();
+        thread::scope(|s| {
+            let (sender, receiver) = channel.split();
+            let t = thread::current();
+
+            s.spawn(move || {
+                sender.send("hello world!");
+                t.unpark();
+            });
+
+            while !receiver.is_ready() {
+                thread::park();
+            }
+
+            assert_eq!(receiver.receive(), "hello world!");
+        });
+    }
+
+    #[test]
+    fn test_block_channel() {
+        let mut channel = BlockChannel::new();
+        thread::scope(|s| {
+            let (sender, receiver) = channel.split();
+
+            s.spawn(move || {
+                sender.send("hello world!");
+            });
+
+            assert_eq!(receiver.receive(), "hello world!");
         });
     }
 }
